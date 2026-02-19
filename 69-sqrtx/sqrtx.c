eeb62902c3554d7cf31698d4e45f9b6abc202239
Swetha Perumalsamy <144234049+swethaperumalsamy@users.noreply.github.com>
2026-02-19 21:37:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (75.94%) - LeetSync

---
+++ 69-sqrtx/sqrtx.c
@@ -0,0 +1,19 @@
+int mySqrt(int x) {
+   if(x<2)
+   return x;
+   int low=1,high=x/2,ans;
+   while(low<=high)
+   {
+    int mid=low+(high-low)/2;
+    if(mid<=x/mid)
+    {
+ans=mid;
+low=mid+1;
+    }
+    else
+    {
+        high=mid-1;
+    }
+   }
+   return ans;
+}
